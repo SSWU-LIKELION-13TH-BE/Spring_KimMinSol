6fe5295049f1e42a2934bbfba4bbdb1c50540be9
MingTori <s090ldbread@gmail.com>
2025-05-06 19:43:23 +0900

    feat: 뺄셈 API

--- src/main/java/com/likelion/session1/service/calculator/CalculatorService.java
+++ src/main/java/com/likelion/session1/service/calculator/CalculatorService.java
@@ -14,4 +14,6 @@ public class CalculatorService {
     public int multiply(int number1, int number2) {
         return number1 * number2;
     }
+
+    public int subtract(int number1, int number2) { return number1 - number2; }
 }
